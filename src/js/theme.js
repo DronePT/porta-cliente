8222d8a863133f317c29d7d547b944b1d61cf217
André Alves <andre.alves@inspireit.pt>
2019-08-29 01:28:49 +0100

    Add base Portal Cliente Back Office structure

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,9 +1,33 @@
 (function($) {
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
   $(window).ready(() => {
     $(".backoffice-header .user").on("dragstart", function(e) {
       e.preventDefault();
 
       return false;
     });
+
+    // fix navigation height
+    const fixNavigationHeight = debounce(function() {
+      const $nav = $(".backoffice-navigation");
+      const $window = $(window);
+
+      if ($nav.height() < $window.height()) {
+        $nav.css("height", $window.height());
+      }
+    }, 50);
+
+    fixNavigationHeight();
+
+    $(window).on("resize", fixNavigationHeight);
   });
 })($);
