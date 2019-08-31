34ac5203a63eb540be409936f62f6c5cd0054424
André Alves <andre.alves@inspireit.pt>
2019-08-31 22:40:04 +0100

    Install ApexcCarts and add charts to Home Page

--- src/js/home.js
+++ src/js/home.js
@@ -15,10 +15,123 @@
 
       if (!$el.data("bg")) return;
 
-      //
       const bgImage = `linear-gradient(to bottom, transparent 40%, rgba(0, 0, 0, .7)), url(${$el.data("bg")})`;
 
       $el.css("background-image", bgImage);
     });
+
+    /**
+     * Build Charts
+     */
+    const $chart = $("#chart-consumos");
+
+    const seriesData = String($chart.data("values") || "")
+      .split(",")
+      .map(v => parseFloat(v));
+
+    const xAxisCategories = $chart.data("labels").split(",");
+
+    var optionsConsumos = {
+      chart: {
+        height: 250,
+        type: "line",
+        fontFamily: "Roboto",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "smooth",
+        width: 3,
+        colors: ["#0075c9"]
+      },
+      series: [
+        {
+          name: "Consumos",
+          data: seriesData
+        }
+      ],
+      grid: {
+        show: true,
+        xaxis: {
+          lines: {
+            show: true
+          }
+        },
+        yaxis: {
+          lines: {
+            show: false
+          }
+        }
+      },
+      xaxis: {
+        categories: xAxisCategories
+      },
+      yaxis: {
+        labels: {
+          show: false
+        }
+      },
+      markers: {
+        size: 5,
+        hover: {
+          size: 7,
+          sizeOffset: 3
+        }
+      }
+    };
+
+    var chart = new ApexCharts($chart[0], optionsConsumos);
+
+    chart.render();
+
+    // Chart Ultima Leitura
+    const $chartLeitura = $("#chart-leitura");
+
+    const leituraSeriesData = String($chartLeitura.data("values") || "")
+      .split(",")
+      .map(v => parseFloat(v));
+
+    var optionsLeitura = {
+      chart: {
+        height: 230,
+        parentHeightOffset: 0,
+        type: "radialBar"
+      },
+      colors: ["#0075c9"],
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 0,
+            size: "68%"
+          },
+          dataLabels: {
+            show: false,
+            name: {
+              fontSize: "28px",
+              fontFamily: "Roboto",
+              color: "#252d34"
+            },
+            value: {
+              show: false
+            },
+            total: {
+              show: false
+            }
+          },
+          track: {
+            background: "#e9ebea"
+          }
+        }
+      },
+      series: leituraSeriesData
+    };
+
+    var chartLeitura = new ApexCharts($chartLeitura[0], optionsLeitura);
+
+    chartLeitura.render();
   });
 })($);
