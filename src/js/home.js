39590b29f1efa1344a14249350df0ea968527252
André Alves <andre.alves@inspireit.pt>
2019-08-31 01:22:30 +0100

    Add Destaques section to Home page

---
+++ src/js/home.js
@@ -0,0 +1,24 @@
+(function($) {
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+  $(window).ready(() => {
+    $(".widget-news").each(function() {
+      const $el = $(this);
+
+      if (!$el.data("bg")) return;
+
+      //
+      const bgImage = `linear-gradient(to bottom, transparent 40%, rgba(0, 0, 0, .7)), url(${$el.data("bg")})`;
+
+      $el.css("background-image", bgImage);
+    });
+  });
+})($);
