5ea18ad5773fdcf1feefda5c239004bd9712787d
André Alves <andre.alves@inspireit.pt>
2019-08-31 01:23:40 +0100

    dist

---
+++ dist/assets/js/theme copy.js
@@ -0,0 +1,33 @@
+"use strict";
+
+(function ($) {
+  var debounce = function debounce(func, delay) {
+    var inDebounce;
+    return function () {
+      var context = this;
+      var args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(function () {
+        return func.apply(context, args);
+      }, delay);
+    };
+  };
+
+  $(window).ready(function () {
+    $(".backoffice-header .user").on("dragstart", function (e) {
+      e.preventDefault();
+      return false;
+    }); // fix navigation height
+
+    var fixNavigationHeight = debounce(function () {
+      var $nav = $(".backoffice-navigation");
+      var $window = $(window);
+
+      if ($nav.height() < $window.height()) {
+        $nav.css("height", $window.height());
+      }
+    }, 150);
+    fixNavigationHeight();
+    $(window).on("resize", fixNavigationHeight);
+  });
+})($);
