c4d5adf25b14f8009c1b0c3fcc2bd33115e1b1d0
André Alves <andre.alves@inspireit.pt>
2019-09-03 00:33:39 +0100

    dist

--- dist/assets/js/faturas.js
+++ dist/assets/js/faturas.js
@@ -17,62 +17,67 @@
     /**
      * Build Charts
      */
-    var $chart = $("#chart-consumos");
-    var seriesData = String($chart.data("values") || "").split(",").map(function (v) {
-      return parseFloat(v);
-    });
-    var xAxisCategories = $chart.data("labels").split(",");
-    var optionsConsumos = {
-      chart: {
-        height: 250,
-        type: "line",
-        fontFamily: "Roboto",
-        zoom: {
-          enabled: false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        curve: "smooth",
-        width: 3,
-        colors: ["#0075c9"]
-      },
-      series: [{
-        name: "Consumos",
-        data: seriesData
-      }],
-      grid: {
-        show: true,
-        xaxis: {
-          lines: {
-            show: true
+    var buildCharts = function buildCharts() {
+      var $chart = $("#chart-consumos");
+      if (!$chart.length) return;
+      var seriesData = String($chart.data("values") || "").split(",").map(function (v) {
+        return parseFloat(v);
+      });
+      var xAxisCategories = $chart.data("labels").split(",");
+      var optionsConsumos = {
+        chart: {
+          height: 250,
+          type: "line",
+          fontFamily: "Roboto",
+          zoom: {
+            enabled: false
           }
         },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: "smooth",
+          width: 3,
+          colors: ["#0075c9"]
+        },
+        series: [{
+          name: "Consumos",
+          data: seriesData
+        }],
+        grid: {
+          show: true,
+          xaxis: {
+            lines: {
+              show: true
+            }
+          },
+          yaxis: {
+            lines: {
+              show: false
+            }
+          }
+        },
+        xaxis: {
+          categories: xAxisCategories
+        },
         yaxis: {
-          lines: {
+          labels: {
             show: false
           }
+        },
+        markers: {
+          size: 5,
+          hover: {
+            size: 7,
+            sizeOffset: 3
+          }
         }
-      },
-      xaxis: {
-        categories: xAxisCategories
-      },
-      yaxis: {
-        labels: {
-          show: false
-        }
-      },
-      markers: {
-        size: 5,
-        hover: {
-          size: 7,
-          sizeOffset: 3
-        }
-      }
+      };
+      var chart = new ApexCharts($chart[0], optionsConsumos);
+      chart.render();
     };
-    var chart = new ApexCharts($chart[0], optionsConsumos);
-    chart.render();
+
+    buildCharts();
   });
 })($);
