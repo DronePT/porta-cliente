0a334df87202a649486f781e673e7790c792cdcd
André Alves <andre.alves@inspireit.pt>
2019-09-11 09:40:12 +0100

    dist

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -20,14 +20,26 @@
     }); // fix navigation height
 
     var fixNavigationHeight = debounce(function () {
-      var $nav = $(".backoffice-navigation");
-      var $window = $(window);
+      var $nav = $(".backoffice-navigation-container");
+      var $window = $("body");
 
       if ($nav.height() < $window.height()) {
         $nav.css("height", $window.height());
       }
     }, 150);
     fixNavigationHeight();
-    $(window).on("resize", fixNavigationHeight);
+    $(window).on("resize", fixNavigationHeight); // toggle main navigation when in mobile view
+
+    $(".main-nav-toggle").on("click", function (event) {
+      event.preventDefault();
+      var $navContainer = $(".backoffice-navigation-container");
+
+      if ($navContainer.hasClass("show")) {
+        $navContainer.removeClass("show");
+        return;
+      }
+
+      $navContainer.addClass("show");
+    });
   });
 })($);
