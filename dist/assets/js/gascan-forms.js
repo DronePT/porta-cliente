9c419a4879dd69b9c218921ec43b64537c826741
André Alves <andre.alves@inspireit.pt>
2019-10-24 18:46:44 +0100

    dist

--- dist/assets/js/gascan-forms.js
+++ dist/assets/js/gascan-forms.js
@@ -1,54 +1,61 @@
 "use strict";
 
 (function (window, document, $) {
-  $(window).ready(function () {
-    /**
-     * Update single form input classes
-     * @param {JQuery<HTMLElement>} $input
-     */
-    var setInputClasses = function setInputClasses($input, event) {
-      var $parent = $input.parent();
+  /**
+   * Update single form input classes
+   * @param {JQuery<HTMLElement>} $input
+   */
+  var setInputClasses = function setInputClasses($input, event) {
+    var $parent = $input.parent();
 
-      if ($input.val().trim().length < 1) {
-        $parent.addClass("is-empty");
-      } else {
-        $parent.removeClass("is-empty");
-      }
+    if ($input.val().trim().length < 1) {
+      $parent.addClass("is-empty");
+    } else {
+      $parent.removeClass("is-empty");
+    }
 
-      if ($input.attr("disabled")) {
-        $parent.addClass("is-disabled");
-      } else {
-        $parent.removeClass("is-disabled");
-      }
+    if ($input.attr("disabled")) {
+      $parent.addClass("is-disabled");
+    } else {
+      $parent.removeClass("is-disabled");
+    }
 
-      switch (event) {
-        case "focus":
-          $parent.addClass("is-focus");
-          break;
+    switch (event) {
+      case "focus":
+        $parent.addClass("is-focus");
+        break;
 
-        case "blur":
-          $parent.removeClass("is-focus");
-          break;
-      }
-    };
+      case "blur":
+        $parent.removeClass("is-focus");
+        break;
+    }
+  };
 
-    var $formInputs = $(".gascan-forms .form-group .form-control"); // iterate every gascan-forms inputs and assign required classes
+  $.fn.gascanForms = function () {
+    this.each(function () {
+      var $el = $(this);
+      var $formInputs = $el.find(".form-group .form-control"); // iterate every gascan-forms inputs and assign required classes
 
-    $.each($formInputs, function (index, element) {
-      var $input = $(element);
-      setInputClasses($input);
-      $input.on("change", function () {
-        setInputClasses($input, "input");
-      });
-      $input.on("input", function () {
-        setInputClasses($input, "input");
-      });
-      $input.on("focus", function () {
-        setInputClasses($input, "focus");
-      });
-      $input.on("blur", function () {
-        setInputClasses($input, "blur");
+      $.each($formInputs, function (index, element) {
+        var $input = $(element);
+        setInputClasses($input);
+        $input.on("change", function () {
+          setInputClasses($input, "input");
+        });
+        $input.on("input", function () {
+          setInputClasses($input, "input");
+        });
+        $input.on("focus", function () {
+          setInputClasses($input, "focus");
+        });
+        $input.on("blur", function () {
+          setInputClasses($input, "blur");
+        });
       });
     });
+  };
+
+  $(window).ready(function () {
+    $(".gascan-forms").gascanForms();
   });
 })(window, document, jQuery);
